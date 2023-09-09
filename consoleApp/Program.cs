61221ded3c054f771c5cd9771b1f8714839a35ee
nesli9 <doanneslihan@gmail.com>
2023-09-09 13:13:41 +0300

    neden değişkenleri kullanırız

--- consoleApp/Program.cs
+++ consoleApp/Program.cs
@@ -1,2 +1,22 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/* Console.WriteLine(5000); //urunA
+Console.WriteLine(6000); //urunB
+Console.WriteLine(7000); //urunC */
+
+/* Console.WriteLine(5000 * 1.18); //urunA KDVsi
+Console.WriteLine(6000 * 1.18); //urunB KDVsi
+Console.WriteLine(7000 * 1.18); //urunC KDVsi */
+
+
+var kdvOrani = 1.08 ;
+var urunA = 5000;
+var urunB = 6000;
+var urunC = 7000;
+var urunD = 5500;
+
+
+
+Console.WriteLine(urunA * kdvOrani); //urunA KDVsi
+Console.WriteLine(urunB * kdvOrani); //urunB KDVsi
+Console.WriteLine(urunC * kdvOrani); //urunC KDVsi
+Console.WriteLine(urunD * kdvOrani); //urunD KDVsi
 
