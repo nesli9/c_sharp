628d9b210f5716c0e5fc579d27b3f8f8ef3c8e7f
nesli9 <doanneslihan@gmail.com>
2023-09-12 18:18:44 +0300

    diziler uygulama

--- consoleApp/Program.cs
+++ consoleApp/Program.cs
@@ -288,7 +288,7 @@
 
 //----- array slicing -----
 
-string[] sehirler = {"zonguldak" , "rize","kocaeli", "istanbul","ankara"};
+// string[] sehirler = {"zonguldak" , "rize","kocaeli", "istanbul","ankara"};
 
 
 //Console.WriteLine(sonuc.Length);
@@ -303,13 +303,80 @@ string[] sehirler = {"zonguldak" , "rize","kocaeli", "istanbul","ankara"};
 //     Console.WriteLine(sehir);
 // }
 
-foreach (var sehir in sehirler[2..])
+// foreach (var sehir in sehirler[2..])
+// {
+//     Console.WriteLine(sehir);
+// }
+
+// string il = "kocaeli";
+
+// Console.WriteLine(il[(..5)]);
+
+
+
+
+// --- Dizi Uygulama ---
+
+// ogrenciler ve notlar isminde 2 tane dizi oluşturunuz ve dizi elemanlarını kullanıcıdan aldığınız değerlerle doldurunuz. (3 öğrenci)
+
+// ogrenciler dizisi kaç elemanlıdır, yazdırınız.
+// ilk 2 öğrencinin ad ve not bilgisini yazdırınız.
+// tüm öğrencilerin not ortalaması nedir? 
+
+
+
+var ogrenciler = new string[3];
+var notlar = new int[3];
+
+Console.Write("1. öğrenci adı: " );
+ogrenciler[0] = Console.ReadLine() ??"";
+
+Console.Write("1. öğrenci notu: " );
+notlar[0] = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("2. öğrenci adı: " );
+ogrenciler[1] = Console.ReadLine() ??"";
+
+Console.Write("2. öğrenci notu: " );
+notlar[1] = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("3. öğrenci adı: " );
+ogrenciler[2] = Console.ReadLine() ??"";
+
+Console.Write("3. öğrenci notu: " );
+notlar[2] = Convert.ToInt32(Console.ReadLine());
+
+
+foreach (var ogrenci in ogrenciler)
 {
-    Console.WriteLine(sehir);
+    Console.WriteLine(ogrenci);
 }
 
-string il = "kocaeli";
+foreach (var not in notlar)
+{
+    Console.WriteLine(not);
+}
 
-Console.WriteLine(il[(..5)]);
+Console.WriteLine(ogrenciler.Length);
+
+
+foreach (var ogrenci in ogrenciler[..2])
+{
+    Console.WriteLine(ogrenci);
+}
+
+foreach (var not in notlar[..2])
+{
+    Console.WriteLine(not);
+}
+
+
+var not1 = notlar[0];
+var not2 = notlar[1];
+var not3 = notlar[2];
+
+var ortalama = (not1 + not2 + not3)/3;
+
+Console.WriteLine(ortalama);
 
 
