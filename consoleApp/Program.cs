cc6ae8b13cfb8f4b91b9548172d2d21c42b196d9
nesli9 <doanneslihan@gmail.com>
2023-09-19 15:46:36 +0300

    for döngüsü

--- consoleApp/Program.cs
+++ consoleApp/Program.cs
@@ -846,13 +846,79 @@
 //         break;
 // }
 
-int sayi = -9;
 
-var sonuc = (sayi % 2 == 0) ? 
-                (sayi > 0) ? "sayı pozitif çift": "sayı negatif çift": 
-                (sayi > 0) ? "sayı pozitif tek": "sayı negatif tek";
 
-Console.WriteLine(sonuc);
+//ternary
+
+// int sayi = -9;
+
+// var sonuc = (sayi % 2 == 0) ? 
+//                 (sayi > 0) ? "sayı pozitif çift": "sayı negatif çift": 
+//                 (sayi > 0) ? "sayı pozitif tek": "sayı negatif tek";
+
+// Console.WriteLine(sonuc);
+
+
+//döngüler
+
+//for
+
+// for (int i = 0; i <= 10; i++) // for(kontrol değişkeni ; kontrol ; güncellemesi)
+// {
+//     Console.WriteLine(i);
+// }
+
+
+// For Döngüsü Uygulamaları
+
+// 1- "1-100" arasındaki sayıların toplamını hesaplayınız.
+
+// var toplam = 0;
+// for (int i = 1; i < 100; i++){
+//     if(i % 2 == 1) {
+//         toplam += i;
+//     }
+//     toplam += i;
+// }
+// Console.WriteLine(toplam);
+
+// 2- Klavyeden girilen başlangıç ve bitiş değerleri arasındaki tüm sayıların toplamını hesaplayınız.
+// Console.Write("başlangıç : ");
+// var baslangıc = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("bitiş   : ");
+// var bitis    = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("artış   : ");
+// var artıs    = Convert.ToInt32(Console.ReadLine());
+
+// var toplam = 0;
+// for (int i = baslangıc ; i < bitis; i= i + artıs){
+//     toplam += i ;
+// }
+
+// Console.WriteLine(toplam);
+
+
+//3- {"ali","ahmet","can","yelda","seda"} dizindeki tüm elemanları ekrana yazdırın.
+
+// string[] isimler = {"ahmet","ali","can","yelda","seda"};
+
+// for (int i = 0; i < isimler.Length; i++){
+//     Console.WriteLine(isimler[i]);
+// }
+
+
+
+// 4- {1,3,4,34,41,56,89} dizisindeki sayılardan hangisileri 3' ün katıdır?
+
+// int[] sayilar = {1,3,4,34,41,56,89} ;
+
+// for (int i = 0; i < sayilar.Length; i++){
+//     if (sayilar[i]%3 == 0){
+//         Console.WriteLine(sayilar[i]);
+//     }
+// }
 
 
 
