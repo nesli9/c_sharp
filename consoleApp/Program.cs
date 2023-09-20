1feeff89b1dd7de12f473da434f5ccaeba036f21
nesli9 <doanneslihan@gmail.com>
2023-09-20 14:09:38 +0300

    dosya ve klasörlerle çalışma

--- consoleApp/Program.cs
+++ consoleApp/Program.cs
@@ -1114,16 +1114,66 @@
 //     sw.WriteLine("c#");
 // }
 
-File.WriteAllText("deneme.txt","merhaba "); //dosya üzerindeki verileri silip üzerine yazar . işi bitince kapatma işini kendi yapr.
-File.AppendAllText("deneme.txt","btk akademi");
+// File.WriteAllText("deneme.txt","merhaba "); //dosya üzerindeki verileri silip üzerine yazar . işi bitince kapatma işini kendi yapr.
+// File.AppendAllText("deneme.txt","btk akademi");
 
 
-using(StreamReader sr = File.OpenText("deneme.txt")) {
-    var s = "";
+// using(StreamReader sr = File.OpenText("deneme.txt")) {
+//     var s = "";
 
-    while((s = sr.ReadLine()) != null) {
-        Console.WriteLine(s);
+//     while((s = sr.ReadLine()) != null) {
+//         Console.WriteLine(s);
+//     }
+// }
+
+
+// Directory.CreateDirectory("temp");
+// Directory.CreateDirectory("temp/deneme");
+// Directory.CreateDirectory("temp/deneme2");
+
+// if (Directory.Exists("temp/deneme2")){
+//     Directory.Delete("temp/deneme2");
+// }else{
+//     Console.WriteLine("silmek istediğiniz klasör yok!"); 
+// }
+
+//string path = @"D:\SİSTEM-SAKIN-SİLME\Desktop\temp";
+// string path = Directory.GetCurrentDirectory() +"/temp"; //proje dosya konumu
+
+// Console.WriteLine(path);
+// Directory.CreateDirectory(path);
+
+
+string rootPath = Directory.GetCurrentDirectory();
+
+// string[] dirs = Directory.GetDirectories(rootPath,"*",SearchOption.TopDirectoryOnly);
+
+// foreach (var dir in dirs){
+//     Console.WriteLine(dir);
+// }
+
+
+string source_path = @"D:\SİSTEM-SAKIN-SİLME\Desktop\Codes\Proje\c_sharp\consoleApp\img";
+string dest_path = @"D:\SİSTEM-SAKIN-SİLME\Desktop\Codes\Proje\c_sharp\consoleApp\images\";
+
+string[] files = Directory.GetFiles(source_path,"*",SearchOption.AllDirectories);
+
+foreach (var file in files){
+    Console.WriteLine(file);
+    Console.WriteLine(Path.GetExtension(file));
+    Console.WriteLine(Path.GetFileNameWithoutExtension(file));
+    Console.WriteLine(Path.GetFileName(file));
+
+    var info = new FileInfo(file);
+    Console.WriteLine($"{Path.GetFileName(file)}: {info.Length}");
+
+    if (!Directory.Exists(dest_path)){
+        Directory.CreateDirectory(dest_path);
     }
+
+    string name = Path.GetRandomFileName()+ Path.GetExtension(file);
+    File.Copy(file, $"{dest_path}{name}");
+
 }
 
 
