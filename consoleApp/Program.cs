14261e3c787442870c6018aee6f1256a73fc4b37
nesli9 <doanneslihan@gmail.com>
2023-09-21 22:53:42 +0300

    debug

--- consoleApp/Program.cs
+++ consoleApp/Program.cs
@@ -1626,6 +1626,45 @@
 // }
 
 
+//hata fırlatma
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+
+
+
+// namespace ConsoleApp{
+//     class Program{
+
+//         static void parola_kontrol(string password){
+//             if (password.Length < 6 || password.Length > 10){
+//                 throw new Exception("parola 6-10 karakter aralığında olmalıdır.");
+//             }
+//             if (!password.Any(char.IsDigit)){
+//                 throw new Exception("parola en az bir rakam içermelidir.");
+//             }
+//             if (!password.Any(char.IsLetter)){
+//                 throw new Exception("parola en az bir harf içermelidir.");
+//             }
+//         }
+//         static void Main(string[] args){
+//             Console.Write("parola :");
+//             string parola = Console.ReadLine();
+
+//             try
+//             {
+//                 parola_kontrol(parola);
+//                 Console.WriteLine("parola geçerli");
+//             }
+//             catch (Exception e)
+//             {
+//                 Console.WriteLine(e.Message);
+//             }
+//         }
+//     }
+// }
+
+
 
 using System;
 using System.Collections.Generic;
@@ -1635,43 +1674,20 @@ using System.Linq;
 
 namespace ConsoleApp{
     class Program{
-
-        static void parola_kontrol(string password){
-            if (password.Length < 6 || password.Length > 10){
-                throw new Exception("parola 6-10 karakter aralığında olmalıdır.");
-            }
-            if (!password.Any(char.IsDigit)){
-                throw new Exception("parola en az bir rakam içermelidir.");
-            }
-            if (!password.Any(char.IsLetter)){
-                throw new Exception("parola en az bir harf içermelidir.");
-            }
-
-
-
-        }
         static void Main(string[] args){
-            Console.Write("parola :");
-            string parola = Console.ReadLine();
+            var sayi1 = 10;
+            var sayi2 = 20;
+            var sonuc = Toplam(sayi1,sayi2);
 
-            try
-            {
-                parola_kontrol(parola);
-                Console.WriteLine("parola geçerli");
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-            
-            
+            Console.WriteLine(sonuc);
+        }
+        static int Toplam(int a , int b){
+            var toplam = a+b;
+            return toplam;
         }
     }
+    
 }
-
-
-
-
 
 
 
