5912d25059c3cee3a42eb8b27245b3537aa09fdc
nesli9 <doanneslihan@gmail.com>
2023-09-21 14:22:07 +0300

    constructors

--- consoleApp/Program.cs
+++ consoleApp/Program.cs
@@ -1222,78 +1222,101 @@
 
 // class - uygulama
 
+// using System;
+
+// namespace ConsoleApp{
+
+//     class Program{
+//         static void Main(string[] args){
+
+//             var soru1 = new Soru(){
+//                 SoruMetni = "Hangisi programlama dili değildir?",
+//                 Secenekler = new string[4]{"python","c#","java","html"},
+//                 Cevap ="html"
+//             };
+//             var soru2 = new Soru(){
+//                 SoruMetni = "Hangisi en popüler programlama dilidir?",
+//                 Secenekler = new string[4]{"python","c#","java","html"},
+//                 Cevap ="c#"
+//             };
+//             var soru3 = new Soru(){
+//                 SoruMetni = "Hangisi web programlama platformu değildir?",
+//                 Secenekler = new string[4]{"django","Asp.net","spring","pyhton"},
+//                 Cevap ="pyhton"
+//             };
+
+//             var sorular = new Soru[] {soru1,soru2,soru3};
+
+//             foreach (var soru in sorular){
+//                 Console.WriteLine(soru.SoruMetni);
+//                 foreach (var secenek in soru.Secenekler){
+//                     Console.WriteLine(secenek);
+//                 }
+
+//                 //kullanıcıdan cevap alma
+//                 Console.Write("cevabınız : ");
+//                 var cevap = Console.ReadLine();
+
+//                 if (soru.cevapKontrol(cevap)){
+//                     Console.WriteLine("doğru cevap");
+//                 }else{
+//                     Console.WriteLine("yanlış cevap");
+//                 }
+//             }
+//         }
+//     }
+//     class Soru{
+//         //properties (sınıfın özellikleri)
+//         public string  SoruMetni { get; set; }
+//         public string[]  Secenekler { get; set; }
+//         public string  Cevap { get; set; }
+
+
+//         //methods (cevap kontrol için)
+//         public bool cevapKontrol(string cevap){
+//             return this.Cevap.ToLower() == cevap.ToLower();
+//         }
+//     }
+// }
+
+
+
+// constructors
+
+
 using System;
 
-namespace ConsoleApp{
+namespace ConsoleApp {
 
-    class Program{
-        static void Main(string[] args){
+    class Program {
 
-            var soru1 = new Soru(){
-                SoruMetni = "Hangisi programlama dili değildir?",
-                Secenekler = new string[4]{"python","c#","java","html"},
-                Cevap ="html"
-            };
-            var soru2 = new Soru(){
-                SoruMetni = "Hangisi en popüler programlama dilidir?",
-                Secenekler = new string[4]{"python","c#","java","html"},
-                Cevap ="c#"
-            };
-            var soru3 = new Soru(){
-                SoruMetni = "Hangisi web programlama platformu değildir?",
-                Secenekler = new string[4]{"django","Asp.net","spring","pyhton"},
-                Cevap ="pyhton"
-            };
-
-            var sorular = new Soru[] {soru1,soru2,soru3};
-
-            foreach (var soru in sorular){
-                Console.WriteLine(soru.SoruMetni);
-                foreach (var secenek in soru.Secenekler){
-                    Console.WriteLine(secenek);
-                }
-
-                //kullanıcıdan cevap alma
-                Console.Write("cevabınız : ");
-                var cevap = Console.ReadLine();
-
-                if (soru.cevapKontrol(cevap)){
-                    Console.WriteLine("doğru cevap");
-                }else{
-                    Console.WriteLine("yanlış cevap");
-                }
-
-
-            }
-
+        static void Main(string[] args) 
+        {
+            var soru1 = new Soru(1, "Hangisi programlama dili değildir?", new string[4] { "Python", "C#", "Java", "Html" }, "Html");
+            var soru2 = new Soru(2, "Hangisi en popüler programlama dilidir?", new string[4] { "Python", "C#", "Java", "Html" }, "C#");
+            var soru3 = new Soru(3, "Hangisi web programlama platformu değildir ?", new string[4] { "Django", "Asp.net", "Spring", "Python" }, "Python");
 
         }
-
     }
-    class Soru{
-        //properties
-        public string  SoruMetni { get; set; }
-        public string[]  Secenekler { get; set; }
-        public string  Cevap { get; set; }
 
-
-        //methods
-        public bool cevapKontrol(string cevap){
+    class Soru {
+        //constructor
+        public Soru(int soruId, string soruMetni, string[] secenekler, string cevap){
+            this.SoruId = soruId;
+            this.SoruMetni = soruMetni;
+            this.Secenekler = secenekler;
+            this.Cevap = cevap;
+        }
+        public int SoruId { get; set; }
+        public string SoruMetni { get; set; }
+        public string[] Secenekler { get; set; }
+        public string Cevap { get; set; }
+        
+        public bool cevapKontrol(string cevap) {
             return this.Cevap.ToLower() == cevap.ToLower();
         }
-
-
     }
-
-
-
-
-
 }
-
-
-
-
 
 
 
