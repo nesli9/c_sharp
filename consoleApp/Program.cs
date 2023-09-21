b3121f5f57059672ac15b269cbefb4e084e5cedc
nesli9 <doanneslihan@gmail.com>
2023-09-21 15:01:46 +0300

    access modifiers

--- consoleApp/Program.cs
+++ consoleApp/Program.cs
@@ -1283,6 +1283,43 @@
 
 // constructors
 
+// using System;
+
+// namespace ConsoleApp {
+
+//     class Program {
+
+//         static void Main(string[] args) 
+//         {
+//             var soru1 = new Soru(1, "Hangisi programlama dili değildir?", new string[4] { "Python", "C#", "Java", "Html" }, "Html");
+//             var soru2 = new Soru(2, "Hangisi en popüler programlama dilidir?", new string[4] { "Python", "C#", "Java", "Html" }, "C#");
+//             var soru3 = new Soru(3, "Hangisi web programlama platformu değildir ?", new string[4] { "Django", "Asp.net", "Spring", "Python" }, "Python");
+
+//         }
+//     }
+
+//     class Soru {
+//         //constructor
+//         public Soru(int soruId, string soruMetni, string[] secenekler, string cevap){
+//             this.SoruId = soruId;
+//             this.SoruMetni = soruMetni;
+//             this.Secenekler = secenekler;
+//             this.Cevap = cevap;
+//         }
+//         public int SoruId { get; set; }
+//         public string SoruMetni { get; set; }
+//         public string[] Secenekler { get; set; }
+//         public string Cevap { get; set; }
+        
+//         public bool cevapKontrol(string cevap) {
+//             return this.Cevap.ToLower() == cevap.ToLower();
+//         }
+//     }
+// }
+
+
+
+//access modifiers (erişim belirteçleri)
 
 using System;
 
@@ -1296,9 +1333,16 @@ namespace ConsoleApp {
             var soru2 = new Soru(2, "Hangisi en popüler programlama dilidir?", new string[4] { "Python", "C#", "Java", "Html" }, "C#");
             var soru3 = new Soru(3, "Hangisi web programlama platformu değildir ?", new string[4] { "Django", "Asp.net", "Spring", "Python" }, "Python");
 
+            Console.WriteLine(soru1.SoruYazdir());
+            Console.WriteLine(soru1.cevapKontrol("html"));
+
+            Console.WriteLine(soru2.SoruYazdir());
+            Console.WriteLine(soru2.cevapKontrol("c#"));
+
+            Console.WriteLine(soru3.SoruYazdir());
+            Console.WriteLine(soru3.cevapKontrol("python"));
         }
     }
-
     class Soru {
         //constructor
         public Soru(int soruId, string soruMetni, string[] secenekler, string cevap){
@@ -1307,21 +1351,25 @@ namespace ConsoleApp {
             this.Secenekler = secenekler;
             this.Cevap = cevap;
         }
-        public int SoruId { get; set; }
-        public string SoruMetni { get; set; }
-        public string[] Secenekler { get; set; }
-        public string Cevap { get; set; }
+        private int SoruId { get; set; }
+        private string SoruMetni { get; set; }
+        private string[] Secenekler { get; set; }
+        private string Cevap { get; set; }
         
+        public string SoruYazdir() {
+            string soru = "";
+            soru += this.SoruMetni + "\n";
+            foreach (var secenek in this.Secenekler){
+                soru += secenek + "\n";
+            }
+
+            return soru;
+        }
         public bool cevapKontrol(string cevap) {
             return this.Cevap.ToLower() == cevap.ToLower();
         }
     }
 }
-
-
-
-
-
 
 
 
