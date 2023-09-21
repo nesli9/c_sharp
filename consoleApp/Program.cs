48d2ad4db107bcb21b50ad9d517ca48226c48f63
nesli9 <doanneslihan@gmail.com>
2023-09-21 15:47:35 +0300

    ArrayList

--- consoleApp/Program.cs
+++ consoleApp/Program.cs
@@ -1409,26 +1409,86 @@
 
 // collections
 
+// using System;
+
+// namespace ConsoleApp{
+//     class Program{
+        
+//         static void Mani(string[] args){
+//             // array (sabit kapasiteye sahiptirler , sadece belirli bir veri tipine bağımlı kalınır (tanımlanan değere))
+//             // int [] sayilar = new int[5];
+//             // sayilar[0] = 10;
+
+//             // Soru[] sorular = new Soru[5];
+//             // sorular[0]=new Soru();
+
+//             //collections (farklı veri tipleri saklanabilir ,dinamik kapasitelidir)
+
+//             //non-generic collections (herhangi bir tipe bağlı kalınmaz)
+//             //array list => int,string
+
+
+
+//             //generic collections (tip tanımlaması gerekir)
+//             //=> int ,string
+
+
+
+            
+//         }
+//     }
+// }
+
+
+//array list
+
+
 using System;
+using System.Collections;
 
 namespace ConsoleApp{
     class Program{
         
-        static void Mani(string[] args){
-            // array
-            int [] sayilar=new int[5];
-            sayilar[0]=10;
+        static void Main(string[] args){
+                //arrayList
+                ArrayList liste = new ArrayList();
+                liste.Add(10);
+                liste.Add("10");
+                liste.Add("ali");
+                liste.Add(null);
+                liste.Add(true);
 
+                var liste2 = new ArrayList(){
+                    5, "ahmet",false,4.5,null
+                };
 
-            //collectios
+                int[] sayilar = {10,20,30};
 
-            
+                liste.AddRange(sayilar);
+
+                var eleman = (int)liste[0];
+                var isim = liste[2].ToString();
+
+                //insert (listeye veri ekleme)
+                liste.Insert(1,"sadık");
+                liste.InsertRange(2,liste2);
+
+                //remove (listeden veri silme)
+                liste.Remove(null);
+                liste.RemoveAt(2);
+                liste.RemoveRange(2,3);
+
+                //contains - indexof
+                Console.WriteLine(liste.Contains(100));
+                Console.WriteLine(liste.IndexOf(10));
+
+                
+                // foreach (var item in liste){
+                //     Console.WriteLine(item);
+                // }
         }
     }
 }
-
-
-
 
 
 
